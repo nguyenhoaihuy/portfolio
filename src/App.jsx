545292ee92069bbf6f9673c78956aa7b0a2285be
Huy <nguyenhoaihuy92@gmail.com>
2022-07-01 16:13:28 -0700

    add components structure, images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import About from './components/about/About'
+import Navbar from './components/navbar/Navbar'
+import Projects from './components/projects/Projects'
+import Stories from './components/stories/Stories'
+import Contact from './components/contact/Contact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <About />
+        <Navbar />
+        <Projects />
+        <Stories />
+        <Contact />
+    </div>
+    
   )
 }
 
