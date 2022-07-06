710e2e349c4f02984b3de7df113c5c6f090b9d68
Huy <nguyenhoaihuy92@gmail.com>
2022-07-05 20:45:52 -0700

    header responsive on iphone

--- src/components/header/Hello.jsx
+++ src/components/header/Hello.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hello() {
   return (
-    <div>
+    <div className='hello'>
         <h3>Hi, I'm</h3>
         <h1>Huy Nguyen</h1>
         <h3 className="text-light">Fullstack Developer</h3>
